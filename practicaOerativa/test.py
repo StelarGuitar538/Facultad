802ccaca6da58c48001da6b2112caeec39e18f34
StelarGuitar538 <danielcarrascosa2005@gmail.com>
2024-05-02 18:47:02 -0300

    po finalizada

--- practicaOerativa/test.py
+++ practicaOerativa/test.py
@@ -1,17 +1,27 @@
 from gestorClientes import gesClientes
 from gestorMovimientos import gesMov
 
+
+
 def test():
+    gm = gesMov()
+    gc = gesClientes()
     while True:
         print("\nMenú de opciones:")
+        print("0. carga")
         print("1. Actualizar saldo y mostrar movimientos de un cliente.")
         print("2. Verificar si un cliente tuvo movimientos por número de tarjeta.")
         print("3. Salir.")
         
         op = input("ingrese opcion")
         
-        if op == "1":
-           gesClientes.listado()
+        if op == "0":
+            gm.inicializar()
+            gc.inicializar()
+        
+        elif op == "1":
+           gc.listado()
+           
         elif op == "2":
-            gesMov.movAbril()
+            gm.movAbril()
         
