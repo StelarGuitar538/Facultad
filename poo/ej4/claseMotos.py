3174cbc56f0f539f4c86b2e3c2aa388223f9d0b8
StelarGuitar538 <danielcarrascosa2005@gmail.com>
2024-05-01 22:20:43 -0300

    ej 4

--- poo/ej4/claseMotos.py
+++ poo/ej4/claseMotos.py
@@ -1,3 +1,4 @@
+import numpy as np
 class Motos:
     __patente: int
     __marca: str
@@ -9,4 +10,24 @@ class Motos:
         self.marca = marca
         self.nya = nya
         self.km = km
+  
+    def __lt__ (self, other):
+        return self.patente < other.patente
+    
+    def getPatente(self):
+        return self.__patente  
+    
+    def mostrarMoto(self):
+        print("patente {}, marca{}, nya{}, km{}", format(self.__patente, self.__marca, self.__nombreYApellidoDelConductor, self.__kilometraje))
+        
+    def getNya(self):
+        return self.__nombreYApellidoDelConductor
+
+    
+        
+            
+    def obtenerDatos(self):
+        nya = input("ingrese nya")
+        if np.where(self.__nombreYApellidoDelConductor == nya):
+            return nya
     
--- poo/ej4/clasePedidos.py
+++ poo/ej4/clasePedidos.py
@@ -6,3 +6,9 @@ class Pedidos:
         self.tiempoDeEntregaEstimado = tiempoDeEntregaEstimado
         self.tiempoDeEntregaReal = tiempoDeEntregaReal
         self.precio = precio
+        
+    def mostrar(self):
+        print(f"{self.patenteMoto}, {self.id}, {self.comidasPedidas}, {self.tiempoDeEntregaEstimado}, {self.tiempoDeEntregaReal}, {self.precio}")
+        
+        
+        
