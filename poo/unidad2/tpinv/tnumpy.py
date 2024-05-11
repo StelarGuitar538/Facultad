834871e79f7ad42f30610317bdfd23013ecad113
StelarGuitar538 <danielcarrascosa2005@gmail.com>
2024-05-10 23:49:55 -0300

    tarea inverstigacion finalizada

--- poo/unidad2/tpinv/tnumpy.py
+++ poo/unidad2/tpinv/tnumpy.py
@@ -5,23 +5,7 @@ class InventarioProductos:
         self.__productosAlmacen = np.array([])
         self.__ventasDiarias = np.array([])
 
-    def hacerCopia(self):
-        self.copiaInventario = np.copy(self.__productosAlmacen)
 
-    def obtenerVistaVentas(self):
-        self.vistaVentas = self.__ventasDiarias[:]
-
-    def agregarVentas(self, ventas):
-        self.juntarVentas = np.concatenate((self.__productosAlmacen, ventas))
-
-    def dividirInventario(self, num_partes):
-        self.dividirInventario = np.array_split(self.__productosAlmacen, num_partes)
-
-    def encontrarValor(self, valor):
-        self.dondeEncontrar = np.where(self.__ventasDiarias == valor)
-
-    def ordenarInventario(self):
-        self.ordenarInventario = np.sort(self.__productosAlmacen)
 
 # Crear una instancia de la clase InventarioProductos
 inventario = InventarioProductos()
