516b1f6c53650db65b6426e5574a01ac8bf32170
StelarGuitar538 <danielcarrascosa2005@gmail.com>
2024-05-13 22:25:11 -0300

    asfae

---
+++ poo/unidad2/practicaOperativa/gestorMovimientos.py
@@ -0,0 +1,32 @@
+from claseMovimientos import Movimientos
+import numpy as np
+import csv
+
+class gesMov:
+    __arregloMov: np.ndarray
+    __dimension: int
+    __cantidad: int
+    __incremento: int
+    
+    def __init__ (self):
+        self.__arregloMov = np.empty([0], dtype=Movimientos)
+        self.__cantidad = 0
+        self.__dimension = 0
+        self.__incremento = 1
+        
+    def agregar(self, m):
+        if self.__dimension == self.__cantidad:
+            self.__dimension += self.__incremento
+            self.__arregloMov.resize(self.__dimension)
+        self.__arregloMov[self.__cantidad] = m
+        self.__cantidad += 1
+        
+    def inicializar(self):
+        archivo = open("MovimientosAbril2024.csv", "r")
+        reader = csv.reader(archivo, delimiter=";")
+        for fila in reader:
+            nMov = Movimientos((fila[0]), (fila[1]), (fila[2]), fila[3], float(fila[4]))
+            self.agregar(nMov)
+        archivo.close()
+        
+        
