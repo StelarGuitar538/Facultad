45e30959caa9f6ee59185d8d202af413c40fff8e
StelarGuitar538 <danielcarrascosa2005@gmail.com>
2024-05-15 17:40:12 -0300

    veloz

--- poo/unidad2/practicaOperativa/claseMovimientos.py
+++ poo/unidad2/practicaOperativa/claseMovimientos.py
@@ -15,7 +15,7 @@ class Movimientos:
     def __str__ (self):
         return f"{self.__fecha}-{self.__desc}-{self.__imp}-{self.__tmov}"
     
-    def getNumTar(self):
+    def getNumTar1(self):
         return self.__numtar
     
     def getFecha(self):
