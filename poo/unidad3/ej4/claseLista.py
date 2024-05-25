64e1b88b0442ae1dd4ebd577f2413cc60fb00f33
StelarGuitar538 <danielcarrascosa2005@gmail.com>
2024-05-25 16:26:01 -0300

    ej 4 u3

--- poo/unidad3/ej4/claseLista.py
+++ poo/unidad3/ej4/claseLista.py
@@ -1,4 +1,7 @@
 from claseNodo import Nodo
+from claseLibros import Libro
+from claseCD import CD
+from clasePublicaciones import Publi
 
 class Lista:
     __comienzo: Nodo
@@ -16,4 +19,44 @@ class Lista:
         while aux != None:
             print(aux.getDato())
             aux = aux.getSig()
-     
+ 
+    def instancia(self):
+        posicion = int(input("ingrese posicion"))
+        aux = self.__comienzo
+        i=0
+        
+        while aux != None and i < posicion:
+            aux = aux.getSig()
+            i+=1
+        if aux is None:
+                print("posicion fuera de rango")
+        else:
+                publi = aux.getDato()
+                if isinstance(publi, Libro):
+                    print(f"La publicación en la posición {posicion} es un Libro.")
+                elif isinstance(publi, CD):
+                     print(f"La publicación en la posición {posicion} es un CD.")
+                else:
+                    print(f"La publicación en la posición {posicion} es de un tipo desconocido.")
+                    
+    def cantPubli(self):
+        cl =0
+        ccd = 0
+        aux = self.__comienzo
+        
+        while aux != None:
+            publi = aux.getDato()
+            if isinstance(publi, Libro):
+                cl+=1
+            elif isinstance(publi, CD):
+                ccd +=1
+            aux = aux.getSig()
+        print(f"la cantidad de libros {cl} y cd {ccd}")
+                
+    
+    def mostrar2(self):
+        aux = self.__comienzo
+        while aux!= None:
+            publi = aux.getDato()
+            print (f"titulo {publi.getTitulo()}, categoria {publi.getCat()} importe de venta {publi.getPrecio()}")
+            aux = aux.getSig()
