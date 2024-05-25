576ec576763d29248bc4175344446d1748307287
StelarGuitar538 <danielcarrascosa2005@gmail.com>
2024-05-25 16:26:01 -0300

    ej 4 u3

--- poo/unidad3/ej4/test.py
+++ poo/unidad3/ej4/test.py
@@ -7,10 +7,17 @@ def test():
     l = Lista()
     cd = CD(130, "pepito", "orgullo y prejuicio", "novela", 35000)
     li = Libro("pepito", "02/04/2019", 200, "orgullo y prejuicio", "novela", 35000)
+    cd1 = CD(130, "juan", "los 3 chanchitos", "infantil", 10000)
+    li1 = Libro("juan", "02/04/2019", 120, "los 3 chanchitos", "infantil", 10000)
+    
     l.agregar(cd)
     l.agregar(li)
+    l.agregar(cd1)
+    l.agregar(li1)
     cd.mostrarCD()
     li.mostrarLi()
-    
-
-    
+    cd1.mostrarCD()
+    li1.mostrarLi()
+    l.instancia()
+    l.cantPubli()
+    l.mostrar2()
