5ba28f18eac61de7ead2b1b059f857c9ff39ffb2
StelarGuitar538 <82880204+StelarGuitar538@users.noreply.github.com>
2024-05-29 12:25:03 +0000

    ej 4 lista python

---
+++ poo/unidad3/ej4 lista/clasePubli.py
@@ -0,0 +1,24 @@
+class Publi:
+    __titulo: str
+    __categoria: str
+    __precio: float
+
+    def __init__ (self, t, c, p):
+        self.__titulo = t
+        self.__categoria = c
+        self.__precio = p
+
+    def mostrarPubli(self):
+        print(f"{self.__titulo} {self.__categoria} {self.__precio}")
+        
+    def getTitulo(self):
+        return self.__titulo 
+    
+    def getCat(self):
+        return self.__categoria
+    
+    def getPrecio(self):
+        return self.__precio
+    
+    def calcularImporte(self):
+        pass
